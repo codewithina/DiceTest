5a06cbf6e7f568159236edd87a567eb7375787ad
Ina Burström <inaburstrom@hotmail.com>
2023-11-06 16:38:49 +0100

    Added random number generator.

--- app/src/main/java/com/example/dicetest/MainActivity.kt
+++ app/src/main/java/com/example/dicetest/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.dicetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val randomNumber = (0 until 7).random()
+        Log.d("!!!", "Slumpat nummer: $randomNumber")
+
     }
 }
