c17d225b50eee6c6a8cee2690a4099e98c5d04f3
Ina Burström <inaburstrom@hotmail.com>
2023-11-06 21:06:06 +0100

    Write out random number in textview.

--- app/src/main/java/com/example/dicetest/MainActivity.kt
+++ app/src/main/java/com/example/dicetest/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.dicetest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var writeOutNumber : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val randomNumber = (0 until 7).random()
-        Log.d("!!!", "Slumpat nummer: $randomNumber")
+        val randomNumber = (1 until 7).random()
 
+        setResult(randomNumber.toString())
+    }
+
+    fun setResult(randomNumber : String){
+        writeOutNumber = findViewById<TextView>(R.id.writeOutResult)
+        writeOutNumber.text = "Tärningen visar nummer $randomNumber"
     }
 }
