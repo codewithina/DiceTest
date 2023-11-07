2a5c4936b9de511e54b6102030aff4fd72553fad
Ina Burström <inaburstrom@hotmail.com>
2023-11-07 09:08:04 +0100

    Writing out the random number when pressing a button.

--- app/src/main/java/com/example/dicetest/MainActivity.kt
+++ app/src/main/java/com/example/dicetest/MainActivity.kt
@@ -3,22 +3,29 @@ package com.example.dicetest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var writeOutNumber : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val randomNumber = (1 until 7).random()
+        val button = findViewById<Button>(R.id.rollTheDice)
+        val writeOutNumber = findViewById<TextView>(R.id.writeOutResult)
 
-        setResult(randomNumber.toString())
+        writeOutNumber.visibility = TextView.INVISIBLE
+
+        button.setOnClickListener {
+            val randomNumber = (1 until 7).random()
+            setResult(randomNumber.toString(), writeOutNumber)
+        }
     }
 
-    fun setResult(randomNumber : String){
-        writeOutNumber = findViewById<TextView>(R.id.writeOutResult)
-        writeOutNumber.text = "Tärningen visar nummer $randomNumber"
+    fun setResult(randomNumber : String, writeOutNumber : TextView){
+        val resultText = getString(R.string.dice_result, randomNumber.toInt())
+        writeOutNumber.text = resultText
+        writeOutNumber.visibility = TextView.VISIBLE
     }
 }
