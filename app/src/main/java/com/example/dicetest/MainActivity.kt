0985bab2f9c87403cf7f4979390d257795023c5c
Ina Burström <inaburstrom@hotmail.com>
2023-11-07 11:47:26 +0100

    Setting the sides of the dice by reading user input.

--- app/src/main/java/com/example/dicetest/MainActivity.kt
+++ app/src/main/java/com/example/dicetest/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -14,11 +15,15 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.rollTheDice)
         val writeOutNumber = findViewById<TextView>(R.id.writeOutResult)
+        val userInputSides = findViewById<EditText>(R.id.enterSidesOfDice)
 
         writeOutNumber.visibility = TextView.INVISIBLE
 
         button.setOnClickListener {
-            val randomNumber = (1 until 7).random()
+            var userInputTxt = userInputSides.text.toString()
+            var sidesOfDice = userInputTxt.toInt() + 1
+
+            val randomNumber = (1 until sidesOfDice).random()
             setResult(randomNumber.toString(), writeOutNumber)
         }
     }
